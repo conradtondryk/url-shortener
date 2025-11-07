a76f9e03ba083c99fb1ca7a0c0d0db18bc34bde1
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-11-07 16:41:56 +0000

    make basic json read/writer

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use std::fs::{self, File};
+use std::io::ErrorKind;
+
+#[derive(Deserialize, Serialize, Parser)]
+struct CliInput {
+    url: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct UrlPair {
+    short_url: String,
+    long_url: CliInput,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Pairs(Vec<UrlPair>);
+
+impl Pairs {
+    fn load() -> Result<Self> {
+        match fs::read_to_string("urls.json") {
+            Ok(data) => serde_json::from_str(&data).map_err(Into::into),
+            Err(e) if e.kind() == ErrorKind::NotFound => Ok(Pairs(vec![])),
+            Err(e) => Err(e.into()),
+        }
+    }
+    fn save(&self) -> Result<()> {
+        serde_json::to_writer(File::create("urls.json")?, &self.0)?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let CliInput { url } = CliInput::parse();
+    let mut list = Pairs::load()?;
+    list.0.push(UrlPair {
+        short_url: format!("ctondryk.dev/{}", rand::thread_rng().gen_range(0..1000000)),
+        long_url: CliInput { url },
+    });
+    list.save()?;
+    Ok(())
 }
